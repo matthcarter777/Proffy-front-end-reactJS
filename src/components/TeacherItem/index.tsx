687db6e1f9a1157b3407c8445eca60d55b7c47bb
mateus <mateushbsb@gmail.com>
2020-08-05 23:04:29 -0300

    finish web version

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
          <header>
-           <img src="https://avatars0.githubusercontent.com/u/49701453?s=460&u=be314b35c1f48dba945a757e7558dbd8c42b451c&v=4" alt="Mateus henrique"/>
+           <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Mateus henrique</strong>
-              <span>Filosofia</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
          </header>
          <p>
-           Tente mover o mundo
-           <br />
-           <br />
-           o primeiro passo será mover a si mesmo.
+           {teacher.bio}
          </p>
 
          <footer>
            <p>
-             Preço/hora
-             <strong>R$ 20,00</strong>
+              Preço/hora
+              <strong>R$ {teacher.cost}</strong>
            </p>
-           <button type="button">
+           <a 
+            target="_blank" 
+            onClick={createNewConnection} 
+            href={`https://wa.me/${teacher.whatsapp}`}
+            >
              <img src={whatsappIcon} alt="Whatsapp"/>
              Entrar em contato
-           </button>
+           </a>
          </footer>
        </article>
   );
